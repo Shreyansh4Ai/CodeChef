47133d6888f408c877ced2c217ca1743d10fafc3
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-05-17 22:44:48 +0530

    Create ChefAndHisApps.cpp

---
+++ 700 above/ChefAndHisApps.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t ;
+    cin>> t ;
+    while(t--){
+        int s , x , y ,z ;
+        cin>> s >> x >> y >> z ;
+
+  
+        int free_space = s - (x + y);
+
+        if (free_space >= z) {
+            cout << 0 << endl;
+        } else {
+            if (s - min(x, y) >= z) {
+                cout << 1 << endl;
+            } else {
+                cout << 2 << endl;
+            }
+        }
+    }
+    return 0;
+}
