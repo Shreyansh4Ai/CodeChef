f1e25e7cb628baa64c38246d1188d45a0f76d58e
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-05-18 09:57:38 +0530

    Add files via upload

---
+++ SPOJ/lifeUniverseAndEverything.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+    while(true){
+        int i;
+        cin>>i;
+         if(i==42){
+            break;
+        }
+        cout<<i<<endl;
+       
+    }
+    return 0 ;
+}
