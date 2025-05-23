bc360b6ce9c17e0193fc9926edc31f96715f089b
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-05-23 23:01:37 +0530

    simpler yet good

---
+++ 700 above/HoopJump.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t ;
+cin>> t ;
+while(t--){
+    long long int n;
+    cin>>n ;
+   
+ 
+  int middle=(n+1)/2;
+  cout<<middle<<endl;
+}
+return 0;
+}
