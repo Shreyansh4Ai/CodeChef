5dc17e7e4c79f320b8d9a9f2b620aaa918c05847
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-06-06 00:06:33 +0530

    easy  , reminds importance of edge cases

---
+++ 1200_above/SmallestPossibleWholeNumber.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t ;
+cin>> t ;
+while(t--){
+    int n , k;
+    cin>> n >> k ;
+    if(k==0) cout << n << endl;
+    else {
+     int ans =  n % k ;
+    cout<<ans<<endl;
+    }
+}
+}
