3d81beb70ffda6edf93678d83626db0e9b1a40b0
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-06-08 22:57:42 +0530

    Create ATMmachine.cpp

---
+++ 700 above/ATMmachine.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ 
+  int t ;
+  cin>> t ; 
+  while(t--){
+  int n , k ;
+  cin>> n >> k ;
+  int a[n];
+  for(int i = 0 ;i < n ; i++){
+      cin>>a[i];
+  }
+  
+  for(int i = 0 ; i< n  ; i++){
+      if(a[i]<= k ){
+           k = k -a[i];
+           cout<<1 ;
+      }else {
+          cout<<0;
+      }
+  }
+  cout<<endl;
+   
+  }
+}
