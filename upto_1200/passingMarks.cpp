99cb53c6bb6823b4a512145f824d5b3d5c0e18a4
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-06-24 22:09:33 +0530

    med

---
+++ upto_1200/passingMarks.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ; 
+	cin >> t ;
+	while ( t--){
+	    int n , x ; 
+	    cin >> n >> x ; 
+	    
+	    vector <int> a(n) ; 
+	    for ( int i = 0 ; i < n ; i++){
+	        cin >> a[i]; 
+	    }
+	    
+	    sort(a.begin(),a.end());
+	    
+	    int y = n - x ; 
+	    
+	    int j = a[y] - 1 ;
+	    cout << j << endl ; 
+	}
+
+}
