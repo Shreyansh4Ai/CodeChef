d7506971c3f19944a81af0437a127e916d0af9d4
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-06-30 23:24:53 +0530

    good problem math

---
+++ upto_1200/oddPairs.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t ;
+	cin>> t ;
+	while(t--){
+	    long long int n ;
+	    cin>> n ;
+	    
+	long long int ans = 2*((n+1)/2)*(n/2);
+	  
+	  cout <<  ans << endl;
+
+
+}
+}
