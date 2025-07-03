644d3780c7c36988127c2c85e9f7937652ff4deb
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-07-03 09:33:48 +0530

    Create rectangleAndSquare.cpp

---
+++ contest/rectangleAndSquare.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a , b , c ;
+    cin>> a >> b >> c ;
+    int rec =  a*b ;
+
+
+
+   if ( rec ==  c*c){
+       cout <<"YES" << endl;
+       
+   }else cout <<"NO"<< endl;
+
+}
