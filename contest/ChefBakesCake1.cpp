bd76dcfba9a0e714fd417410a6a5e451917ab076
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-07-14 23:01:18 +0530

    easy

---
+++ contest/ChefBakesCake1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+int n , m ;
+cin >> n >> m ;
+
+int tcost = n*30 ;
+int sell = m*50 ;
+
+int profit = sell - tcost ;
+
+cout <<profit << endl;
+}
