2faf5c535ccd6b4368bbe3deb678c4bb974207d1
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-07-30 23:24:08 +0530

    redo

---
+++ upto_1200/adjacentSumParity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; string s;
+        cin >> n >> s;
+
+        if (s[0] == s[n - 1]) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
