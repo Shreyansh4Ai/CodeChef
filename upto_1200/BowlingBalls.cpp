02150e87b372739c7a548b401eb94bbedd063243
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-07-31 20:51:23 +0530

    start197

---
+++ upto_1200/BowlingBalls.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t ;
+	cin>> t ;
+	while(t--){
+	    int n , x , y ;
+	    cin >> n >> x >> y ;
+	    vector<int>a(n);
+	    for(int  i = 0 ; i< n ; i++){
+	        cin>>a[i];
+	    }
+	    int count =0 ;
+	    for(int i = 0 ; i< n ; i++){
+	        if (a[i] >= x && a[i]<=y){
+	            count++;
+	        }
+	    }
+	    cout<<count<<endl;
+	    
+	    
+	    
+	    
+	}
+return 0 ;
+}
