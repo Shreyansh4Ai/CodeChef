7f8b528b8c5adce19ed3572a654c8a17da47c218
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-08-06 23:45:15 +0530

    start198

---
+++ upto_1200/makeCatPractice.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+string s ;
+cin >> s ;
+if(s == "cat" || s == "act" || s =="tac" || s=="tca"|| s=="cta" || s=="atc") cout << "YES"<<endl;
+else cout << "NO"<<endl;
+}
