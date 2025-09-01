48c7362b24b25a8c776463eb55063e2a34227604
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-09-01 23:43:14 +0530

    Create moneymaking.cpp

---
+++ upto_1200/moneymaking.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int X, Y;
+    cin >> X >> Y;
+
+    long long total = 1LL * X * 5000 + 1LL * Y * 9800;
+    cout << total << "\n";
+
+    return 0;
+}
