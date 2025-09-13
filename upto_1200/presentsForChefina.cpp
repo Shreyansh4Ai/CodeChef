59af7c598de706c523ea4889f6f93558d739528b
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-09-13 22:54:09 +0530

    good

---
+++ upto_1200/presentsForChefina.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t ;
+cin >> t ;
+while(t--){
+    long long int n ;
+    cin >> n ;
+    
+    int ans = n - n/5;
+    cout << ans << endl;
+    
+}
+
+}
