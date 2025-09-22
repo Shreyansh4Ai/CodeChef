b5ebe4f743924da0b8d1d7649d3cface35a660ea
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-09-22 23:49:40 +0530

    good

---
+++ upto_1200/BinaryBattles.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, A, B;
+        cin >> N >> A >> B;
+        int rounds = log2(N);
+        int total_time = rounds * A + (rounds - 1) * B;
+        cout << total_time << '\n';
+    }
+    return 0;
+}
