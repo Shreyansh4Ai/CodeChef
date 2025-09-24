fb6710dea970d0c64f24a1c78c234bbda63ef2fe
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-09-24 23:51:57 +0530

    start205

---
+++ upto_1200/cuteStrings.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s ;
+	cin >> s ;
+	if(s[0] == s[2]){
+	if (s[1] == 'w')  cout << "cute"<< endl;
+	   
+else cout << "NO"<<endl;
+}
+else {
+    cout << "NO"<< endl;
+}
+}
