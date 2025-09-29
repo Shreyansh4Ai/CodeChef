5282a4e0e35ef6def8bc11914c71c995cf564d69
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-09-29 23:01:36 +0530

    good

---
+++ upto_1200/chefAndWaterBottles.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t ;
+	cin >> t ;
+	while(t--){
+	    int n , x , k ;
+	    cin >> n >> x >> k ;
+
+	    int ans = min(n , k/x);
+	        cout << ans << endl;
+	}
+
+}
