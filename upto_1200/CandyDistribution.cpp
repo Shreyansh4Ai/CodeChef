387d9c0f5b192c5b721f546dc7bfe194f792cf7b
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-09-30 10:51:30 +0530

    easy

---
+++ upto_1200/CandyDistribution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t ;
+cin >> t ;
+while(t--){
+    int n , m ;
+    cin >> n >> m ;
+        int l = n / m ;
+        if( l % 2 == 0 && n % m == 0) cout <<"YES"<<endl;
+        else cout <<"NO"<<endl;
+}
+
+}
