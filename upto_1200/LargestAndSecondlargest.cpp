35c842db8757a4ccc2b67b5662ddc45ea7255380
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-10-01 11:40:52 +0530

    good

---
+++ upto_1200/LargestAndSecondlargest.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int largest = INT_MIN;
+        int secondLargest = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] > largest) {
+                secondLargest = largest;
+                largest = a[i];
+            } else if (a[i] > secondLargest && a[i] != largest) {
+                secondLargest = a[i];
+            }
+        }
+
+        cout << largest + secondLargest << endl;
+    }
+    return 0;
+}
