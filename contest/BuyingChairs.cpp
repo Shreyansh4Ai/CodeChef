86b08315ea58a3f655588c3458fb7bd43248a297
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-06 22:19:38 +0530

    start211 greedy approach

---
+++ contest/BuyingChairs.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t ;
+cin >> t ;
+while(t--){
+    int w , p , k  ;
+    cin >> w >> p >> k ;
+    
+  int wnum = min(w , k );
+   int styl =  wnum + k ;
+   cout << styl << endl;
+  
+
+}
+}
