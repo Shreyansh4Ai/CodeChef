e56d21c49e2ae3e8b15a4106f1f6890425f99dbd
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-16 00:09:03 +0530

    Update ChefAndProportion.cpp

--- upto_1200/ChefAndProportion.cpp
+++ upto_1200/ChefAndProportion.cpp
@@ -1,21 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+#include<bits/stdc++.h>
+using namespace std ;
 
-bool isProportion(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    do {
-        int x = nums[0], y = nums[1], z = nums[2], w = nums[3];
-        if (x * w == y * z) return true;
-    } while (next_permutation(nums.begin(), nums.end()));
-    return false;
-}
-
-int main() {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-    vector<int> nums = {a, b, c, d};
-    cout << (isProportion(nums) ? "Possible" : "Impossible") << endl;
-    return 0;
+int main(){
+    int a , b , c , d ;
+    cin >>a  >> b >>  c >> d ;
+    
+        
+    if((a*b == c*d) || (a*c == b*d ) || (a*d == b*c))
+    cout <<"POSSIBLE"<<endl;
+    else cout <<"IMPOSSIBLE"<<endl;
 }
