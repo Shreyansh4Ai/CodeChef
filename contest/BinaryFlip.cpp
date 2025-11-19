45ca46cbb106a0239905a16085d5aff18ec65d19
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-19 18:32:12 +0530

    good math based revise

--- contest/BinaryFlip.cpp
+++ contest/BinaryFlip.cpp
@@ -1,29 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std ;
-using ll = long long ;
-                 //sahi krna h 
+
 int main(){
     int  t;
     cin >> t ;
     while(t--){
-        ll n ;
+        int n ;
         cin >> n ;
         string s ;
         cin >> s ;
-      
-        ll cnt00 = 0 ;
-        ll cnt11 = 0 ;
-    for(int  i = 0 ; i<n-1 ; i+=2){
-        if(s[i] == '0' && s[i+1] == '0') cnt00++ ;
-        else if(s[i] == '1'  && s[i+1] == 1) cnt11++;
 
+        int cnt00 = 0 ; 
+        int cnt11 = 0 ;
+        for(int i = 0 ; i<n-1 ; i++){
+            if(s[i] == s[i+1]){
+                if(s[i] == '1') cnt11++;
+                else cnt00++;
+            }
+        }
+        cout << max((cnt00 - cnt11 +1)/2 , 0) << endl;
     }
-
-    if(cnt00 >= cnt11){
-        cout << cnt00 - cnt11 << endl;
-    }else {
-           cout << 0 << endl; 
-    }
-
-    }
+    return 0 ;
 }
