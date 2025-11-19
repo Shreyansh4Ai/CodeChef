45541958ad802bc9cf4b94c813c903a1277674c9
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-19 18:08:29 +0530

    hard

---
+++ contest/ExitDoor.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+
+        int ans = 0 ;
+        
+        for(int i = 0 ; i<n ; i++){
+            int left = 0 , right = 0 ;
+            
+            for(int j = 0 ; j<i ; j++){
+                if(a[j] < a[i])left++;
+            }
+            for(int j= i+1; j<n ; j++){
+                if(a[j]<a[i]) right++;
+            }
+            ans +=min(left,right);
+        }
+        
+           cout << ans << endl;
+        }
+     
+
+}
