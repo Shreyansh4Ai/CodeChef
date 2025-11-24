fac2a5cfca5deb92fe189dcdb6163a75faaedf41
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-24 23:41:03 +0530

    Implement AvgOfThree program in C++

---
+++ upto_1200/AvgOfThree.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t ;
+	cin >> t ;
+	while(t--){
+	    int x ;
+	    cin >> x ;
+	    
+	    int a , b ,c ;
+	    a=b=c=x;
+	    
+	    cout << a-1 <<" "<< b <<" "<< c+1 << endl;
+	     
+	    
+	}
+
+}
