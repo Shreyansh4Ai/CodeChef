ecc43e00aa5f61c6cddd2133b86abe0452a4dcdc
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-12-04 19:26:04 +0530

    start215

---
+++ contest/compOf4.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x ;
+	cin >> x ;
+	if(x == 1) cout << 8000;
+	else if (x== 2) cout << 4000;
+	else if(x==3) cout<< 2000;
+	else if(x == 4) cout << 1000 ;
+
+}
